9430ea447dee6355d2d1a7dd410ab45960260f85
Tuurash <turasmimoo@gmail.com>
2020-07-11 22:14:56 +0600

    photo upload done

--- VlogSite/VlogSite/Controllers/HomeController.cs
+++ VlogSite/VlogSite/Controllers/HomeController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VlogSite.Models;
+using VlogSite.Models.ViewModel;
 using VlogSite.Repository;
 
 namespace VlogSite.Controllers
@@ -17,9 +20,12 @@ namespace VlogSite.Controllers
 
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IWebHostEnvironment WebHostEnvironment;
+
+        public HomeController(ILogger<HomeController> logger,IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            WebHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -36,7 +42,7 @@ namespace VlogSite.Controllers
         [HttpPost]
         public ActionResult Register(UserTbl usr)
         {
-            IRepository<UserTbl> userRep = new UserRepo();
+   
             userRep.Insert(usr);
             return RedirectToAction("Login");
         }
@@ -46,6 +52,59 @@ namespace VlogSite.Controllers
             return View();
         }
 
+
+        public IActionResult Index2()
+        {
+            
+            return View(userRep.GetAll());
+        }
+
+        public IActionResult Register2()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+
+        public IActionResult Register2(userViewModel usrVm)
+        {
+            BlogContext bc = new BlogContext();
+
+            
+            string FileName = UploadFile(usrVm);
+
+            var user = new UserTbl
+            {
+                UserName= usrVm.UserName,
+                UserEmail=usrVm.UserEmail,
+                UserPassword=usrVm.UserPassword,
+                UserPhoto = FileName
+            };
+            bc.UserTbl.Add(user);
+            bc.SaveChanges();
+
+            return RedirectToAction("Index2");
+        }
+
+        private string UploadFile(userViewModel usrVm)
+        {
+            
+
+            string fileName = null;
+
+            if (usrVm.UserPhoto != null)
+            {
+                string uploadDir = Path.Combine(WebHostEnvironment.WebRootPath,"Image");
+                fileName = Guid.NewGuid().ToString() + "-" + usrVm.UserPhoto.FileName;
+                string filePath = Path.Combine(uploadDir, fileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    usrVm.UserPhoto.CopyTo(fileStream);
+                }
+            }
+            return fileName;
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- VlogSite/VlogSite/Models/ViewModel/userViewModel.cs
+++ VlogSite/VlogSite/Models/ViewModel/userViewModel.cs
@@ -1,18 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
-using System.Web;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace VlogSite.Models
+namespace VlogSite.Models.ViewModel
 {
-    public partial class UserTbl
+    public class userViewModel
     {
         public int UserId { get; set; }
         public string UserName { get; set; }
         public string UserEmail { get; set; }
         public string UserPassword { get; set; }
         public string UserAbout { get; set; }
-        public string UserPhoto { get; set; }
-
         
+       
+        public IFormFile UserPhoto { get; set; }
     }
 }
