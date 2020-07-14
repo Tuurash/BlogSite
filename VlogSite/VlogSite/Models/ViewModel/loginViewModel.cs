8cefcf74ab0363b72b46fab34858210aaab1ae41
Tuurash <turasmimoo@gmail.com>
2020-07-14 11:55:12 +0600

    login model

---
+++ VlogSite/VlogSite/Models/ViewModel/loginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VlogSite.Models.ViewModel
+{
+    public class loginViewModel
+    {
+        [Required(ErrorMessage = "Must type User Email correctly!")]
+        public string userMail { get; set; }
+
+        [Required(ErrorMessage = "Must Type Password correctly!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+    }
+}
